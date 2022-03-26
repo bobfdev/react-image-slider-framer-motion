8d8cc24e4c62be83ba4186ebeb877b55636beb8d
Bob F <bobfdev@gmail.com>
2022-03-25 23:40:11 -0400

    updated slider constraints

--- src/App.js
+++ src/App.js
@@ -7,15 +7,22 @@ import { useRef, useEffect, useState } from 'react';
 import images from './images-data';
 
 function App() {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef()
+
+  useEffect(() => {
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
+  }, [])
+
   return (
-    <Carousel>
+    <Carousel ref={carousel}>
       <InnerCarousel 
         drag='x'
-        dragConstraints={{ right: 0 }}
+        dragConstraints={{ right: 0, left: -width }}
       >
         {images.map((image) => {
           return(
-            <ImageCarousel>
+            <ImageCarousel key={image}>
               <img src={image} alt='' />
             </ImageCarousel>
           );
